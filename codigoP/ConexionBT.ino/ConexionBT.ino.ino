07c104dc809426091e87e8f8e648ff109e76f96d
MarvEdCV <71089107+MarvEdCV@users.noreply.github.com>
2022-03-07 16:05:09 -0600

    Update
    Programa conexion simple BT.

---
+++ codigoP/ConexionBT.ino/ConexionBT.ino.ino
@@ -0,0 +1,27 @@
+/* CODIGO PRUEBA EDUARDO */
+int led=13;
+char entrada;
+/*FIN EDUARDO*/
+
+void setup() {
+  // put your setup code here, to run once:
+   //INICIO EDUARDO
+   pinMode(13,OUTPUT); //Declaramos el pin como salida
+  //FIN EDUARDO
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   //INICIO EDUARDO
+    if(Serial.available()>0){
+      entrada = Serial.read();
+    }
+    if(entrada=='E'){;
+      digitalWrite(led,HIGH);
+    }
+    if(entrada=='A'){
+      digitalWrite(led,LOW);
+    }
+  //FIN EDUARDO
+}
